524323e1198f7132c70d921e2d72dde513601ae6
opsec-infosec <74446520+opsec-infosec@users.noreply.github.com>
2021-07-31 13:50:29 +0400

    Create ex01.c

---
+++ ex01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+	printf("Ok... so you are still here\n"); 
+	printf("Are you sure you know what you are doing?\n");
+  printf("Just in case you didn't understand what RTFM stands for\n");
+  printf("Just a hint... it does not stand for: Required Training For Muggles\n");
+
+    return (0);
+}
