5b030e13228edb7ca56607d62d2fee3584c5ece9
opsec-infosec <74446520+opsec-infosec@users.noreply.github.com>
2021-07-31 13:43:40 +0400

    Create ex00.c

---
+++ ex00.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+	printf("Calling non_existing()\n"); 
+	printf("You should read the subject.pdf\n");
+  printf("But since you are here... the answer is: 42\n");
+  printf("Expected Answer:  RTFM\n");
+
+    return (0);
+}
