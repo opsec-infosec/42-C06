0f16ea939bca6db1f65fde02f6a91274b2b26307
opsec-infosec <74446520+opsec-infosec@users.noreply.github.com>
2021-07-31 14:05:18 +0400

    Update ex02.c

--- ex02.c
+++ ex02.c
@@ -2,9 +2,10 @@
 
 int main(void) {
 
-  printf("Ok... Since you like compiling these\n");
-  printf("Here is a nice quote for you:\n);
-	printf("So long, and thanks for all the fish.\n"); 
+  	printf("Ok... Since you like compiling these\n");
+  	printf("Here is a nice quote for you:\n);
+	printf("So long, and thanks for all the fish.\n");
+	printf("Good Luck from opinfosec in May Piscine!!!!\n");
 
     return (0);
 }
