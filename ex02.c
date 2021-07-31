7f2a22f4c8d55111d1d0f47928295ee6d59b42ef
opsec-infosec <74446520+opsec-infosec@users.noreply.github.com>
2021-07-31 13:57:46 +0400

    Create ex02.c

---
+++ ex02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+  printf("Ok... Since you like compiling these\n");
+  printf("Here is a nice quote for you:\n);
+	printf("So long, and thanks for all the fish.\n"); 
+
+    return (0);
+}
+
+
